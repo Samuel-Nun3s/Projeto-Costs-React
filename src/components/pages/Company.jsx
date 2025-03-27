c6aa71db81ddc49e3e1edb6469b101385d1a1dcd
Samuel Nunes <samuelsalomao883@gmail.com>
2025-03-26 23:07:24 -0300

    Estruturando o projeto

---
+++ src/components/pages/Company.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+function Company() {
+  return (
+    <h1>Tudo certo aqui na Company</h1>
+  )
+}
+
+export default Company;
---
+++ src/components/pages/NewProject.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+function NewProject() {
+  return(
+    <h1>Tudo certo aqui na NewProject</h1>
+  )
+}
+
+export default NewProject;
