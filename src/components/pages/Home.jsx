f39374b92ade6638057b0acc9fe5de410bfc5f0a
Samuel Nunes <samuelsalomao883@gmail.com>
2025-03-28 08:23:43 -0300

    Home

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,8 +1,18 @@
 import React from "react";
+import styles from './Home.module.css';
+
+import LinkButton from "../layout/LinkButton";
+
+import savings from '../../img/savings.svg';
 
 function Home() {
   return (
-    <h1>Tudo certo aqui na Home</h1>
+    <section className={styles.home_container}>
+      <h1>Bem vindo ao <span>Costs</span></h1>
+      <p>Comece a gerenciar os seus projetos agora mesmo</p>
+      <LinkButton to="/newproject" text="Criar Projeto"/>
+      <img src={savings} alt="Costs" />
+    </section>
   )
 }
 
