576f3ebc5f608a5067d102a5bba1136df229df04
Samuel Nunes <samuelsalomao883@gmail.com>
2025-04-02 08:19:44 -0300

    Pagina de edição de dados

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Company from './components/pages/Company';
 import Contact from './components/pages/Contact';
 import NewProject from './components/pages/NewProject';
 import Projects from './components/pages/Projects';
+import Project from './components/pages/Project';
 
 import Navbar from './components/layout/Navbar';
 import Footer from './components/layout/Footer';
@@ -20,6 +21,7 @@ function App() {
             <Route path='/company' element={<Company />}></Route>
             <Route path='/contact' element={<Contact />}></Route>
             <Route path='/newproject' element={<NewProject />}></Route>
+            <Route path='/project/:id' element={<Project />}></Route>
           </Routes>
         </Container>
         <Footer />
