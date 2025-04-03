a19434817d48d7502f7ea29da8ce19694c27c27f
Samuel Nunes <samuelsalomao883@gmail.com>
2025-04-03 08:24:18 -0300

    Exibindo os serviços

--- src/components/pages/Project.jsx
+++ src/components/pages/Project.jsx
@@ -9,11 +9,13 @@ import Container from '../layout/Container';
 import Message from '../layout/Message';
 import ProjectForm from '../project/ProjectForm';
 import ServiceForm from '../service/ServiceForm';
+import ServiceCard from '../service/ServiceCard';
 
 function Project() {
   const { id } = useParams();
 
   const [project, setProject] = useState([]);
+  const [services, setServices] = useState([]);
   const [showProjectForm, setShowProjectForm] = useState(false);
   const [showServiceForm, setShowServiceForm] = useState(false);
   const [message, setMessage] = useState();
@@ -95,10 +97,14 @@ function Project() {
     })
       .then((resp) => resp.json())
       .then((data) => {
-        // exibir os serviços
-        console.log(data);
+        setServices(data.services);
+        setShowServiceForm(false);
       })
       .catch(err => console.log(err));
+  }
+
+  function removeService() {
+
   }
 
   function toggleProjectForm() {
@@ -159,7 +165,21 @@ function Project() {
             </div>
             <h2>Serviços</h2>
             <Container customClass="start">
-              <p>Itens de Serviços</p>
+              {services.length > 0 &&
+                services.map((service) => (
+                  <ServiceCard 
+                    id={service.id}
+                    name={service.name}
+                    cost={service.cost}
+                    description={service.description}
+                    key={service.id}
+                    handleRemove={removeService}
+                  />
+                ))
+              }
+              {services.length === 0 && <p>Não há serviços cadastrados.</p>
+
+              }
             </Container>
           </Container>
         </div>
