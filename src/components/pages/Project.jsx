ec2d032ccb3290e1c727ce67f9b4ab6d93aed6b7
Samuel Nunes <samuelsalomao883@gmail.com>
2025-04-05 20:22:13 -0300

    Removendo serviços

--- src/components/pages/Project.jsx
+++ src/components/pages/Project.jsx
@@ -32,6 +32,7 @@ function Project() {
         .then(resp => resp.json())
         .then((data) => {
           setProject(data);
+          setServices(data.services);
         })
         .catch((err) => console.log(err));
     }, 300);
@@ -103,8 +104,31 @@ function Project() {
       .catch(err => console.log(err));
   }
 
-  function removeService() {
+  function removeService(id, cost) {
+    const servicesUpdated = project.services.filter(
+      (service) => service.id !== id
+    )
 
+    const projectUpdated = project;
+
+    projectUpdated.services = servicesUpdated;
+
+    projectUpdated.cost = parseFloat(projectUpdated.cost) - parseFloat(cost);
+
+    fetch(`http://localhost:5000/projects/${projectUpdated.id}`, {
+      method: 'PATCH',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(projectUpdated)
+    })
+      .then(resp => resp.json())
+      .then((data) => {
+        setProject(projectUpdated);
+        setServices(servicesUpdated);
+        setMessage('Serviço removido com sucesso!');
+      })
+      .catch(err => console.log(err));
   }
 
   function toggleProjectForm() {
